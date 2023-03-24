49a56db3b5551c9cab8e2bf81a26166501f42e55
Dhwani31 <dhwanihansoti315@gmail.com>
2023-03-23 23:19:27 -0400

    registered models in admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.db import models
+from .models import Amenities,Hotel,HotelImage,HotelBooking
+# Register your models here.
+
 
 # Register your models here.
+admin.site.register(Amenities)
+admin.site.register(Hotel)
+admin.site.register(HotelImage)
+admin.site.register(HotelBooking)
